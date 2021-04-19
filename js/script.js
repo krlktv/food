e74e96138a21592811708825f97cbe4f87d5514e
Kirill Kotov <krlktv@mail.ru>
2021-04-19 17:54:06 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function hideTabContent() {
 		tabsContent.forEach(item => {
-			item.style.display = 'none';
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
 		});
 		tabs.forEach(item => {
 			item.classList.remove('tabheader__item_active');
@@ -16,7 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	function showTabContent(i = 0) {
-		tabsContent[i].style.display = 'block';
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
 		tabs[i].classList.add('tabheader__item_active');
 	}
 
@@ -27,7 +29,12 @@ window.addEventListener('DOMContentLoaded', () => {
 		const target = event.target;
 
 		if (target && target.classList.contains('tabheader__item')) {
-
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
 		}
 	});
 });
