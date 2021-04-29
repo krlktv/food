c9a79eb312118db953b33e64e9cb864ef9a8fafc
Kirill Kotov <krlktv@mail.ru>
2021-04-29 22:22:42 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -93,4 +93,23 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	setClock('.timer', deadline);
+
+	// Modal
+
+	const modalTrigger = document.querySelector('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
+
+	modalTrigger.addEventListener('click', () => {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+	});
+
+	modalCloseBtn.addEventListener('click', () => {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	});
+
 });
