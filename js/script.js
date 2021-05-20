658d1f5131414ad8df415b0e1c81855ccece4b3d
Kirill Kotov <krlktv@mail.ru>
2021-05-20 16:34:36 +0300

    update

--- js/script.js
+++ js/script.js
@@ -219,6 +219,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		failure: 'Что-то пошло не так...'
 	};
 
+	forms.forEach(item => {
+		postData(item);
+	});
+
 	function postData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
@@ -226,21 +230,36 @@ window.addEventListener('DOMContentLoaded', () => {
 			const statusMessage = document.createElement('div');
 			statusMessage.classList.add('status');
 			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
 
 			const request = new XMLHttpRequest();
 			request.open('POST', 'server.php');
 
-			request.setRequestHeader('Cotent-type', 'multipart/form-data');
+			request.setRequestHeader('Cotent-type', 'application/json');
 			const formData = new FormData(form);
 
-			request.send(formData);
+			const object = {};
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
 
 			request.addEventListener('load', () => {
 				if (request.status === 200) {
 					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.failure;
+
 				}
 			});
 		});
 	}
-
 });
